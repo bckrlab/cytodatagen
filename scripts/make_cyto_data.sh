601150d2e4b80f874c9716bd5fe55fbef008d008
Bjarne Hiller <bjarne.hiller@uni-rostock.de>
2025-08-14 15:29:10 +0000

    add notebooks

--- scripts/make_cyto_data.sh
+++ scripts/make_cyto_data.sh
@@ -1,11 +1,17 @@
 #!/usr/env/bin bash
 
-# generate data from command line
 python -m cytodatagen \
-    --add-switch-effect \
-    --add-signal-effect \
-    --add-batch-effect \
-    --add-noise-effect \
-    --add-sinh-effect \
-    -o artifacts/cytodata --format fcs \
+    -o artifacts/cytodata --format h5ad \
     --verbose
+
+if [ false ]; then
+    # generate data from command line
+    python -m cytodatagen \
+        --add-switch-effect \
+        --add-signal-effect \
+        --add-batch-effect \
+        --add-noise-effect \
+        --add-sinh-effect \
+        -o artifacts/cytodata --format fcs \
+        --verbose
+fi
